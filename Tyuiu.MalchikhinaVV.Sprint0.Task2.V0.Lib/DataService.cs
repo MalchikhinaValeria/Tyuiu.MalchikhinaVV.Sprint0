4e432a73be1c772c16afa2188480099bdb1632da
Home <Home@LAPTOP-L43VJUUA>
2025-09-21 20:41:10 +0500

    Task2

---
+++ Tyuiu.MalchikhinaVV.Sprint0.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MalchikhinaVV.Sprint0.Task2.V0.Lib
+{
+    public class DataService
+    {
+        public static string GetMessage(string name)
+        {
+            return $"Привет, {name}";
+        }
+    }
+}
---
+++ Tyuiu.MalchikhinaVV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MalchikhinaVV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.MalchikhinaVV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+
+            var name = "Лера";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Лера", res);
+
+        }
+    }
+}
