308a1d5949c4e9535062d41421f7af5d9468b4ef
Home <Home@LAPTOP-L43VJUUA>
2025-09-22 00:04:59 +0500

    Task7

---
+++ Tyuiu.MalchikhinaVV.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MalchikhinaVV.Sprint0.Task7.V0.Lib;
+namespace Tyuiu.MalchikhinaVV.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArraysValid()
+        {
+            int[] nums1 = { 1, 1, 1, 1, 1, };
+            int[] nums2 = { 1, 1, 1, 1, 1, };
+            int[] nums3 = { 2, 2, 2, 2, 2, };
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
+
+        }
+    }
+}
